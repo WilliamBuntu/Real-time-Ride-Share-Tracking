775c3020632184a8441307b120a58ad7a766a91e
NG-Solennel <gisubizongabosolennel@gmail.com>
2024-04-27 22:02:21 +0200

    Finished prod

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ import {
   Input,
   SkeletonText,
   Text,
-} from '@chakra-ui/react'
-import { FaLocationArrow, FaTimes } from 'react-icons/fa'
+} from "@chakra-ui/react";
+import { FaLocationArrow, FaTimes } from "react-icons/fa";
 
 import {
   useJsApiLoader,
@@ -17,86 +17,143 @@ import {
   Marker,
   Autocomplete,
   DirectionsRenderer,
-} from '@react-google-maps/api'
-import { useRef, useState } from 'react'
+  InfoWindow,
+} from "@react-google-maps/api";
+import { useRef, useState } from "react";
+import { markers } from "./markers";
 
-
-
-const center = { lat: -1.939826787816454, lng: 30.0445426438232  }
+const center = { lat: -1.939826787816454, lng: 30.0445426438232 };
 
 function App() {
-  console.log(process.env.REACT_APP_GOOGLE_MAPS_API_KEY)
+  const [currentLocation, setCurrentLocation] = useState({
+    lat: center.lat,
+    lng: center.lng,
+  });
+  if ("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition(
+      function (position) {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        setCurrentLocation({ lat: latitude, lng: longitude });
+      },
+      function (error) {
+        switch (error.code) {
+          case error.PERMISSION_DENIED:
+            console.error("User denied the request for Geolocation.");
+            break;
+          case error.POSITION_UNAVAILABLE:
+            console.error("Location information is unavailable.");
+            break;
+          case error.TIMEOUT:
+            console.error("The request to get user location timed out.");
+            break;
+          case error.UNKNOWN_ERROR:
+            console.error("An unknown error occurred.");
+            break;
+          default:
+            console.error("An unknown error occurred.");
+            break;
+        }
+      }
+    );
+  } else {
+    // Geolocation is not supported
+    console.error("Geolocation is not supported by this browser.");
+  }
+
   const { isLoaded } = useJsApiLoader({
-    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY ,
-    libraries: ['places'],
-  })
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    libraries: ["places"],
+  });
 
+  const [activeMarker, setActiveMarker] = useState(null);
+  const [origin, setOrigin] = useState("");
 
+  const handleActiveMarker = (marker) => {
+    if (marker === activeMarker) {
+      return;
+    }
+    setActiveMarker(marker);
+  };
 
-  const [map, setMap] = useState(/** @type google.maps.Map */ (null))
-  const [directionsResponse, setDirectionsResponse] = useState(null)
-  const [distance, setDistance] = useState('')
-  const [duration, setDuration] = useState('')
-
-  
+  const [map, setMap] = useState(/** @type google.maps.Map */ (null));
+  const [directionsResponse, setDirectionsResponse] = useState(null);
+  const [distance, setDistance] = useState("");
+  const [duration, setDuration] = useState("");
 
   /** @type React.MutableRefObject<HTMLInputElement> */
-  const originRef = useRef()
+  const originRef = useRef();
   /** @type React.MutableRefObject<HTMLInputElement> */
-  const destiantionRef = useRef()
+  const destiantionRef = useRef();
 
   if (!isLoaded) {
-    return <SkeletonText />
+    return <SkeletonText />;
   }
 
   async function calculateRoute() {
-    if (originRef.current.value === '' || destiantionRef.current.value === '') {
-      return
+    if (originRef.current.value === "" || destiantionRef.current.value === "") {
+      return;
     }
     // eslint-disable-next-line no-undef
-    const directionsService = new google.maps.DirectionsService()
+    const directionsService = new google.maps.DirectionsService();
     const results = await directionsService.route({
       origin: originRef.current.value,
       destination: destiantionRef.current.value,
       // eslint-disable-next-line no-undef
       travelMode: google.maps.TravelMode.DRIVING,
-    })
-    setDirectionsResponse(results)
-    setDistance(results.routes[0].legs[0].distance.text)
-    setDuration(results.routes[0].legs[0].duration.text)
+    });
+    setDirectionsResponse(results);
+    setDistance(results.routes[0].legs[0].distance.text);
+    setDuration(results.routes[0].legs[0].duration.text);
   }
 
   function clearRoute() {
-    setDirectionsResponse(null)
-    setDistance('')
-    setDuration('')
-    originRef.current.value = ''
-    destiantionRef.current.value = ''
+    setDirectionsResponse(null);
+    setDistance("");
+    setDuration("");
+    originRef.current.value = "";
+    destiantionRef.current.value = "";
   }
 
   return (
     <Flex
-      position='relative'
-      flexDirection='column'
-      alignItems='center'
-      h='100vh'
-      w='100vw'
+      position="relative"
+      flexDirection="column"
+      alignItems="center"
+      h="100vh"
+      w="100vw"
     >
-      <Box position='absolute' left={0} top={0} h='100%' w='100%'>
+      <Box position="absolute" left={0} top={0} h="100%" w="100%">
         {/* Google Map Box */}
         <GoogleMap
           center={center}
           zoom={12}
-          mapContainerStyle={{ width: '100%', height: '100%' }}
+          mapContainerStyle={{ width: "100%", height: "100%" }}
           options={{
             zoomControl: false,
             streetViewControl: false,
             mapTypeControl: false,
             fullscreenControl: false,
           }}
-          onLoad={map => setMap(map)}
+          onLoad={(map) => setMap(map)}
         >
-          <Marker position={center} />
+          {markers.map(({ id, name, position }) => (
+            <Marker
+              key={id}
+              position={position}
+              onClick={() => {
+                handleActiveMarker(id);
+                setOrigin(`${position.lat},${position.lng}`);
+              }}
+            >
+              {activeMarker === id ? (
+                <InfoWindow onCloseClick={() => setActiveMarker(null)}>
+                  <div>{name}</div>
+                </InfoWindow>
+              ) : null}
+            </Marker>
+          ))}
+
           {directionsResponse && (
             <DirectionsRenderer directions={directionsResponse} />
           )}
@@ -104,56 +161,77 @@ function App() {
       </Box>
       <Box
         p={4}
-        borderRadius='lg'
+        borderRadius="lg"
         m={4}
-        bgColor='white'
-        shadow='base'
-        minW='container.md'
-        zIndex='1'
+        bgColor="white"
+        shadow="base"
+        minW="container.md"
+        zIndex="1"
       >
-        <HStack spacing={2} justifyContent='space-between'>
+        <HStack spacing={2} justifyContent="space-between">
           <Box flexGrow={1}>
             <Autocomplete>
-              <Input type='text' placeholder='Origin' ref={originRef} />
+              <Input
+                type="text"
+                placeholder="Origin"
+                ref={originRef}
+                value={origin}
+                onChange={(e) => setOrigin(e.target.value)}
+              />
             </Autocomplete>
           </Box>
           <Box flexGrow={1}>
             <Autocomplete>
-              <Input
-                type='text'
-                placeholder='Destination'
-                ref={destiantionRef}
-              />
+              <select id="select-marker" ref={destiantionRef}>
+                <option value="-1.9355377074007851,30.060163829002217">
+                  Stop A
+                </option>
+                <option value="-1.9358808342336546,30.08024820994666">
+                  Stop B
+                </option>
+                <option value="-1.9489196023037583,30.092607828989397">
+                  Stop C
+                </option>
+                <option value="-1.9592132952818164,30.106684061788073">
+                  Stop D
+                </option>
+                <option value="-1.9487480402200394,30.126596781356923">
+                  Stop E
+                </option>
+                <option value="-1.9365670876910166,30.13020167024439">
+                  Ending point (Kimironko)
+                </option>
+              </select>
             </Autocomplete>
           </Box>
 
           <ButtonGroup>
-            <Button colorScheme='pink' type='submit' onClick={calculateRoute}>
+            <Button colorScheme="pink" type="submit" onClick={calculateRoute}>
               Calculate Route
             </Button>
             <IconButton
-              aria-label='center back'
+              aria-label="center back"
               icon={<FaTimes />}
               onClick={clearRoute}
             />
           </ButtonGroup>
         </HStack>
-        <HStack spacing={4} mt={4} justifyContent='space-between'>
+        <HStack spacing={4} mt={4} justifyContent="space-between">
           <Text>Distance: {distance} </Text>
           <Text>Duration: {duration} </Text>
           <IconButton
-            aria-label='center back'
+            aria-label="center back"
             icon={<FaLocationArrow />}
             isRound
             onClick={() => {
-              map.panTo(center)
-              map.setZoom(15)
+              map.panTo(currentLocation);
+              map.setZoom(15);
             }}
           />
         </HStack>
       </Box>
     </Flex>
-  )
+  );
 }
 
-export default App
+export default App;
